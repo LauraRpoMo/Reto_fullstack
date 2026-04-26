ad116e02dc960eaa576d18270f65921e92004d4a
LauraReMo1001 <laura.remo@outlook.com>
2026-04-26 02:46:55 -0500

    feat: registro de usuarios

--- src/components/organisms/Header.jsx
+++ src/components/organisms/Header.jsx
@@ -1,8 +1,12 @@
 import { useProductStore } from "../../store/productStore";
+import { useUserStore } from "../../store/userStore";
 
 function Header() {
   const searchTerm = useProductStore((state) => state.searchTerm);
   const setSearchTerm = useProductStore((state) => state.setSearchTerm);
+
+  const currentUser = useUserStore((state) => state.currentUser);
+  const logoutUser = useUserStore((state) => state.logoutUser);
 
   return (
     <header className="flex flex-col md:flex-row justify-between items-center px-8 py-4 border-b gap-4">
@@ -21,6 +25,22 @@ function Header() {
         <a href="#">Productos</a>
         <a href="#">Carrito</a>
       </nav>
+
+      <div className="flex items-center gap-4">
+        {currentUser ? (
+          <>
+            <span>Hola, {currentUser.name}</span>
+            <button
+              onClick={logoutUser}
+              className="px-4 py-2 bg-black text-white rounded-xl"
+            >
+              Logout
+            </button>
+          </>
+        ) : (
+          <span>No has iniciado sesión</span>
+        )}
+      </div>
     </header>
   );
 }
