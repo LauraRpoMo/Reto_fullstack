94b2166afa2d390d17f73573e946808f608e6de0
LauraReMo1001 <laura.remo@outlook.com>
2026-04-26 01:39:04 -0500

    feat: mockdata de categorias

---
+++ src/mockdata/categories.js
@@ -0,0 +1,22 @@
+export const mockCategories = [
+  {
+    id: 1,
+    name: "Electrónica",
+    slug: "electronics"
+  },
+  {
+    id: 2,
+    name: "Moda",
+    slug: "fashion"
+  },
+  {
+    id: 3,
+    name: "Accesorios",
+    slug: "accessories"
+  },
+  {
+    id: 4,
+    name: "Hogar",
+    slug: "home"
+  }
+];
